df560c7ddf8a87e46ce635428511be7108f86d3f
Elisha Otobi <eotobi@gmail.com>
2022-09-04 07:40:44 +0300

    complete homepage

--- frontend/src/pages/homepage/homepage.jsx
+++ frontend/src/pages/homepage/homepage.jsx
@@ -1,18 +1,71 @@
 import React from 'react'
 import Sidebar from '../widgets/sidebar/sidebar';
+import FacebookIcon from '@mui/icons-material/Facebook'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import Navbar from '../widgets/navbar/navbar';
 import './homepage.scss'
 
 function Homepage() {
   return (
     <div className='mainhome'>
-      <div className="sidebar">
+      <div className="home-sidebar">
         <Sidebar/>
       </div>
-      <div className="nav-home">
-        <div className="navbar">
-          <Navbar/>
-            This is main home content
+      <div className="home-content">
+        <div className="home-title">
+          <div className="intro-title">
+            <h1>Hi,</h1>
+            <h1>I'm Elisha,</h1>
+            <h1> Fullstack Web and Mobile Developer</h1>
+            <div className="skills">React JS / Python Django / Flutter / Kotlin / Freelancer</div>
+            <div className='contact-btn'>Contact Me</div>
+          </div>
+          <div className="social-media">
+            <div className="fb">
+              <FacebookIcon className="fb-icon"/>
+              <h6>Facebook</h6>
+            </div>
+            <div className="fb">
+              <GitHubIcon className="git-icon"/>
+              <h6>GitHub</h6>
+            </div>
+            <div className="fb">
+              <TwitterIcon className="twt-icon"/>
+              <h6>Twitter</h6>
+            </div>
+            <div className="fb">
+              <LinkedInIcon className="lnkd-icon"/>
+              <h6>LinkedIn</h6>
+            </div>
+          </div>
+        </div>
+        <div className="details-card">
+        <img src={process.env.PUBLIC_URL + '/mylogo.svg'} alt=""/>
+        <pre className='code'>
+          {
+          `
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("World")
+    }
+}
+
+          `
+          }
+        </pre>
+        <div className='details'>
+          I design and create modern, user-oriented, attractive UX 
+          and develop secure, scalable and efficient backends.
+        </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/widgets/sidebar/sidebar.jsx
+++ frontend/src/pages/widgets/sidebar/sidebar.jsx
@@ -1,59 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ArchitectureIcon from '@mui/icons-material/Architecture'
 import DehazeIcon from '@mui/icons-material/Dehaze'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import ConstructionIcon from '@mui/icons-material/Construction'
 import AutoStoriesIcon from '@mui/icons-material/AutoStories'
 import DataObjectIcon from '@mui/icons-material/DataObject'
-import DashboardIcon from '@mui/icons-material/Dashboard'
 import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+import HomeIcon from '@mui/icons-material/Home';
 import "./sidebar.scss"
 
+
 function Sidebar() {
+  const [isOpen, setIsopen] =  useState(false);
+
+  const ToggleSidebar = () =>{
+    isOpen===true? setIsopen(false) : setIsopen(true);
+    // console.log(isOpen)
+  }
   return (
-    <div className='sidebar'>
+    <div className={`sidebar${isOpen == true ?'-active' : ''}`}>
       <div className="logo-content">
         <div className="logo">
           <ArchitectureIcon className="icon"/>
           <div className="logo-name">Artbit Apps</div>
         </div>
-        <DehazeIcon className="menu-button" id="btn" />
+        <DehazeIcon className="menu-button" id="btn" onClick={ToggleSidebar}/>
       </div>
       <ul className="nav-list">
+      <li>
+          <a href="#">
+          <HomeIcon className="icons"/>
+            <span className="links-name">Homepage</span>
+          </a>
+          <span className="tooltip">view homepage</span>
+        </li>
         <li>
           <a href="#">
           <AccountCircleIcon className="icons"/>
             <span className="links-name">Profile Details</span>
           </a>
-          {/* <span className="toolstip">DASHBOARD</span> */}
+          <span className="tooltip">view my profile</span>
         </li>
         <li>
           <a href="#">
           <ConstructionIcon className="icons"/>
             <span className="links-name">Personal Projects</span>
           </a>
-          {/* <span className="toolstip">DASHBOARD</span> */}
+          <span className="tooltip">view my projects</span>
         </li>
         <li>
           <a href="#">
           <AutoStoriesIcon className="icons"/>
             <span className="links-name">Articles</span>
           </a>
-          {/* <span className="toolstip">DASHBOARD</span> */}
+          <span className="tooltip">read my articles</span>
         </li>
         <li>
           <a href="#">
           <DataObjectIcon className="icons"/>
             <span className="links-name">Tutorials</span>
           </a>
-          {/* <span className="toolstip">DASHBOARD</span> */}
-        </li>
-        <li>
-          <a href="#">
-          <DashboardIcon className="icons"/>
-            <span className="links-name">Dashboard</span>
-          </a>
-          {/* <span className="toolstip">DASHBOARD</span> */}
+          <span className="tooltip">coding tutorials</span>
         </li>
       </ul>
       <div className="profile-content">
